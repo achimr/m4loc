e31f8b55d83aa626cce548d034b929bb5bad01f4
AchimRu <AchimRu@a49a3cff-31e9-edaf-9553-3f9cc7bf7cb2>
2011-03-01 20:20:11 +0000

    Improved pseudo translation with specifyable phrase length and shuffling
    git-svn-id: https://m4loc.googlecode.com/svn/trunk@47 a49a3cff-31e9-edaf-9553-3f9cc7bf7cb2

--- xliff/pseudo_translate.pl
+++ xliff/pseudo_translate.pl
@@ -22,14 +22,49 @@
 #
 
 use strict;
+use Getopt::Std;
+use List::Util qw(shuffle);
 
 binmode(STDIN,":utf8");
 binmode(STDOUT,":utf8");
 
+our $opt_n;
+getopts("n:");
+if(@ARGV != 0) {
+    die "Usage: $0 [-n <max phrase length>] < <tokenized lowercased source file> > <pseudo translated output file>\n";
+}
+my $max_phrase_len = $opt_n?$opt_n:1;
+
+# print "Maximum phrase length: $max_phrase_len\n";
+
 while(<>) {
     chomp;
-    print _translate_text_pig_latin($_);
+    my @tokens = split;
+    # Create array of start/end hashes with maximum length $phrase_len
+    if(@tokens) {
+	my $i = 0;
+	my @phrase_index;
+	while($i <= $#tokens) {
+	    # Conditional needed because rand(0) is special cased as rand(1) according to documentation
+	    my $rand_pl = $max_phrase_len==1 ? 0 : int(rand($max_phrase_len));
+	    my $phrase_end = $i+$rand_pl < $#tokens ? $i+$rand_pl : $#tokens;
+	    push @phrase_index, {"s"=>$i,"e"=>$phrase_end};
+	    $i = $phrase_end+1;
+	} 
+
+	# Shuffle start/end array
+	my @shuffled_phrases = shuffle(@phrase_index);
+
+	# Output pig latin translations and traces
+	foreach my $phrase (@shuffled_phrases) {
+	    print _translate_text_pig_latin(join(' ',@tokens[$phrase->{'s'}..$phrase->{'e'}]));
+	    print ' ';
+	    print '|'.$phrase->{'s'}.'-'.$phrase->{'e'}.'| ';
+	}
+    }
+    # New line if there were tokens or not
     print "\n";
+
 }
 
 # This sub is used when you want to debug everything in this script except the
@@ -37,6 +72,7 @@ while(<>) {
 #
 # This subroutine was written by Herve Saint-Amand
 # and copied from translate.cgi in the Moses codebase
+# insertion of fake traces removed
 
 sub _translate_text_pig_latin {
     my ($text) = @_;
@@ -46,11 +82,6 @@ sub _translate_text_pig_latin {
         ($2 eq lc $2 ? lc $1 : $1) .
         'ay';
     /gei;
-
-    # insert fake traces
-    my $i = -1;
-    $text .= ' ';
-    $text =~ s/\s+/$i++; " |$i-$i| "/ge;
 
     return $text;
 }

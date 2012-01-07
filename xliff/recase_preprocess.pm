2fcfc7067e6f433a58196c2437d5e5823cd60634
achimru <achimru@a49a3cff-31e9-edaf-9553-3f9cc7bf7cb2>
2012-01-07 16:48:55 +0000

    Modulinos for recase pre-processing and pseudo translation
    git-svn-id: https://m4loc.googlecode.com/svn/trunk@110 a49a3cff-31e9-edaf-9553-3f9cc7bf7cb2

---
+++ xliff/recase_preprocess.pm
@@ -0,0 +1,68 @@
+#!/usr/bin/perl -w
+package recase_preprocess;
+
+run() unless caller();
+
+#
+# Remove Moses traces (phrase alignment info) from translated text
+# in preparation for correcting upper-/lowercase with the recaser
+#
+# Copyright 2011,2012 Digital Silk Road
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+use strict;
+
+sub run {
+    binmode(STDIN,":utf8");
+    binmode(STDOUT,":utf8");
+    $|++;
+
+    while(<>) {
+	chomp;
+	print remove_trace($_),"\n";
+    }
+}
+
+sub remove_trace {
+    my $traced = shift;
+
+    $traced =~ s/\|\d+\-\d+\|\s*//g;
+    return $traced;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+recase_preprocess.pl: Remove Moses traces from translated text
+
+=head1 USAGE
+
+    perl recase_preprocess.pl < target_with_traces > target_tokenized
+
+Script to remove Moses traces (phrase alignment info) from translated text
+in preparation for correcting upper-/lowercase with the recaser. Moses traces -
+phrase alignment information enclosed in vertical bars C<|start-end|> - are
+required to reinsert XLIFF inline element markup back into the text. However,
+when applying a recaser model, the traces negatively impact the upper- and
+lowercasing. To avoid this, the traces can be temporarily removed before
+recasing and then reinserted with C<recase_postprocess.pl>. 
+
+Input is expected to be UTF-8 encoded (without a leading byte-order 
+mark U+FEFF) and output will be UTF-8 encoded as well. 
+

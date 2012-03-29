59695ad73deae0c5032061d2e09bff2cb8b5dc7d
AchimRu <AchimRu@a49a3cff-31e9-edaf-9553-3f9cc7bf7cb2>
2012-03-29 14:53:21 +0000

    Made m4loc_tag subclass of m4loc to reduce code duplication
    git-svn-id: https://m4loc.googlecode.com/svn/trunk@140 a49a3cff-31e9-edaf-9553-3f9cc7bf7cb2

--- xliff/m4loc_tag.pm
+++ xliff/m4loc_tag.pm
@@ -1,6 +1,8 @@
 #!/usr/bin/perl -w
 
 package m4loc_tag;
+use m4loc;
+our @ISA = qw(m4loc);
 
 __PACKAGE__->run(@ARGV) unless caller();
 
@@ -26,67 +28,40 @@ __PACKAGE__->run(@ARGV) unless caller();
 use warnings;
 use strict;
 
-use FindBin qw($Bin);
-use Getopt::Std;
+use File::Basename;
+use File::Spec qw(rel2abs);
 use IPC::Open2;
 use wrap_tokenizer;
 use wrap_detokenizer;
-use fix_markup_ws;
 use wrap_markup;
 use decode_markup;
 
-# Class Methods
-sub run {
-    ref(my $class= shift) and die "Class name needed";
+# Class Methods - inherited
 
-    # Always flush buffers
-    $|++;
-    binmode(STDIN,":utf8");
-    binmode(STDOUT,":utf8");
-
-    my %opts;
-    getopts("s:l:k:d:m:r:",\%opts);
-
-    # Source language
-    my $sl = $opts{s} ? $opts{s} : "fr";
-
-    # Target language
-    my $tl = $opts{t} ? $opts{t} : "en";
-
-    # Tokenizer configuration
-    my $tok_prog = $opts{k} ? $opts{k} : "$Bin/tokenizer.pm";
-    my @tok_param = ("-l",$sl);
-
-    # Detokenizer program
-    my $detok_prog = $opts{d} ? $opts{d} : "$Bin/detokenizer.pm";
-    my @detok_param = ("-l",$tl);
-
-    # Moses configuration 
-    my $moses_config = $opts{m} ? $opts{m} : "$Bin/moses.ini";
-
-    # Recaser configuration file
-    my $recaser_config = $opts{r} ? $opts{r} : "$Bin/recaser.ini";
-
-    my $inlinetextmt = $class->new($sl,$tl,$tok_prog,\@tok_param,$detok_prog,\@detok_param,$moses_config,$recaser_config);
-    while(my $source = <STDIN>){
-	chomp $source;
-	print $inlinetextmt->translate_tag($source),"\n";
-    }
-}
-
-# Constructor
+# Constructor 
+# Cannot be inherited b/c Moses is started with different parameters
 sub new {
     ref(my $class= shift) and die "Class name needed";
     my $sourcelang = shift;
     my $targetlang = shift;
+    my $moses_config = shift;
+    my $recaser_config = shift;
     my $tok_prog = shift;
     my $tok_param_ref = shift;
     my $detok_prog = shift;
     my $detok_param_ref = shift;
-    my $moses_config = shift;
-    my $recaser_config = shift;
     
     # New tokenizer and detokenizer objects
+    # Use __FILE__ to determine library directory
+    my ($myfile,$mydir) = fileparse(File::Spec->rel2abs(__FILE__));
+    if(!$tok_prog) {
+	$tok_prog = "$mydir/tokenizer.pm";
+	$tok_param_ref = ['-l',$sourcelang];
+    }
+    if(!$detok_prog) {
+	$detok_prog = "$mydir/detokenizer.pm";
+	$detok_param_ref = ['-l',$targetlang];
+    }
     my $tokenizer = wrap_tokenizer->new($tok_prog, @{$tok_param_ref});
     my $detokenizer = wrap_detokenizer->new($detok_prog, @{$detok_param_ref});
 
@@ -114,27 +89,8 @@ sub new {
     return $self;
 }
 
-sub DESTROY {
-    my $self = shift;
-
-    close $self->{MosesIn};
-    close $self->{MosesOut};
-    my $exitpid = waitpid($self->{MosesPid},0);
-    my $childstatus = $? >> 8;
-    if($childstatus) {
-	warn "Error in closing child Moses process: $childstatus\n";
-    }
-    close $self->{RecaseIn};
-    close $self->{RecaseOut};
-    $exitpid = waitpid($self->{RecasePid},0);
-    $childstatus = $? >> 8;
-    if($childstatus) {
-	warn "Error in closing child recaser process: $childstatus\n";
-    }
-}
-
 # Object Methods
-sub translate_tag {
+sub translate {
     my $self = shift;
     if(!ref $self) {
 	return "Unnamed $self";
